94e68e0ba34ff25686369e0efaa6fce7af498296
APinkDSLite <nryoakum@gmail.com>
2022-12-02 01:55:34 -0800

    reduced max level
    unmuted

--- Battle.py
+++ Battle.py
@@ -7,8 +7,8 @@ import sys
 button_a = digitalio.DigitalInOut(board.BUTTON_A)
 button_a.switch_to_input(pull=digitalio.Pull.DOWN)
 
-levelexpthresholds = {1: 100, 2: 225}#, 3: 350, 4: 475, 5: 600, 6: 725}
-levelstats = [{'level': 1, 'hp': 100, 'atk': 10},{'level': 2, 'hp': 110, 'atk': 15}]#{'level': 3, 'hp': 125, 'atk': 25},{'level': 4, 'hp': 140, 'atk': 30},{'level': 5, 'hp': 150, 'atk': 40},{'level': 6, 'hp': 170, 'atk': 45}]
+levelexpthresholds = {1: 100, 2: 225, 3: 350,}
+levelstats = [{'level': 1, 'hp': 100, 'atk': 10},{'level': 2, 'hp': 110, 'atk': 15},{'level': 3, 'hp': 125, 'atk': 25}]
 monster_list = [{'name': 'Goblin', 'hp': 50, 'atk': 10, 'exp_drop': 25},{'name': 'Skeleton', 'hp': 75, 'atk': 15, 'exp_drop': 40},{'name': 'Slime', 'hp': 25, 'atk': 5, 'exp_drop': 10}, {'name': 'Minotaur', 'hp': 125, 'atk': 25, 'exp_drop': 100}, {'name': 'Werewolf', 'hp': 155, 'atk': 20, 'exp_drop': 120}]
 
 class BattleHandler:
